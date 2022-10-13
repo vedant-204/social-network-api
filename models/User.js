36ab3154f6ba229b792f6ce307237de63e3ad646
vedant-204 <katyayana2003@gmail.com>
2022-10-13 11:30:10 +0530

    added models for mongo

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        min: 3,
+        max: 20,
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+    },
+    followers: {
+        type: Array,
+        default: [],
+    },
+    followings: {
+        type: Array,
+        default: [],
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
+
+module.exports = mongoose.model("User", userSchema);
