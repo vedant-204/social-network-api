09af96310f0f6a2cb8bbe83f19c09b6801b8e25d
vedant-204 <katyayana2003@gmail.com>
2022-10-13 11:29:40 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -3,14 +3,23 @@ const mongoose = require('mongoose');
 const helmet = require('helmet');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const userRoute = require('./routes/users');
+const authRoute = require('./routes/auth');
+
+const app = express();
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, {userNewUrlParser: true, useUnifiedTopology: true}, () => {
+mongoose.connect(process.env.MONGO_URL, {userNewUrlParser: true}, () => {
     console.log("Connected to MongoDB");
 })
 
-const app = express();
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute);
+app.use("/api/authenticate", authRoute);
 
 app.listen(3000, () => {
   console.log('Server on http://localhost:3000');
